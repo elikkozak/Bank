20c8dd6ac2b2ad1fb0ca45283314715dd1873aad
elikkozak <elik.kozak@gmail.com>
2022-11-15 20:49:26 +0200

    created basic server

---
+++ backend/sever.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+import uvicorn
+
+
+app = FastAPI()
+
+
+@app.get("/")
+def root():
+    return "server is up and running"
+
+
+if __name__ == "__main__":
+    uvicorn.run("server:app", host="localhost", port=8000, reload=True)
