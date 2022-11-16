767352f653322a16411026df3295b58f7b8b1ed7
elikkozak <elik.kozak@gmail.com>
2022-11-16 18:25:07 +0200

    initalized the componentes with empty function component

---
+++ frontend/src/components/Breakdown.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Breakdown() {
+    return (
+      <div className="breakdown">
+        
+      </div>
+    );
+  }
---
+++ frontend/src/components/TransactionOperation.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function TransactionOperation() {
+    return (
+      <div className="transaction-operations">
+        
+      </div>
+    );
+  }
