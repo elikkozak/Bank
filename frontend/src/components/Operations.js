e711886b2465fec29ce3cf88fe4faeb0212c636b
elikkozak <elik.kozak@gmail.com>
2022-11-19 18:40:53 +0200

    removed fake transaction insert and created a form that adds transaction with css

--- frontend/src/components/Operations.js
+++ frontend/src/components/Operations.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import "../styles/operations.css";
 
 export default function Operations() {
   const [operationInputs, setOperationInputs] = useState({
@@ -21,22 +22,35 @@ export default function Operations() {
     });
   };
 
+  const handleOperation = function (event) {
+    let transaction = { ...operationInputs };
+    transaction["amount"] =
+      event.target.name === "withdraw"
+        ? -parseInt(transaction.amount)
+        : parseInt(transaction.amount);
+    addTransaction(transaction);
+  };
+
   return (
     <div className="operations-container">
-      <div className="inputs">
+      <h2>Insert transactions</h2>
+      <div className="inputs-container">
         <input
+          className="input"
           value={operationInputs.amount}
           name="amount"
           placeholder="Transaction amount"
           onChange={inputHandler}
         ></input>
         <input
+          className="input"
           value={operationInputs.vendor}
           name="vendor"
           placeholder="Transaction vendor"
           onChange={inputHandler}
         ></input>
         <input
+          className="input"
           value={operationInputs.category}
           name="category"
           placeholder="Transaction category"
@@ -45,10 +59,18 @@ export default function Operations() {
       </div>
 
       <div className="buttons-container">
-        <button name="deposit" onClick={addTransaction}>
+        <button
+          className="deposit-button"
+          name="deposit"
+          onClick={handleOperation}
+        >
           Deposit
         </button>
-        <button name="withdraw" onClick={addTransaction}>
+        <button
+          className="withdraw-button"
+          name="withdraw"
+          onClick={handleOperation}
+        >
           Withdraw
         </button>
       </div>
