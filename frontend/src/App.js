2e79df53807241bd60b63fe5e5e824923ce8af3c
elikkozak <elik.kozak@gmail.com>
2022-11-19 17:06:57 +0200

    created navbar with linking and css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,17 @@
 import "./App.css";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import Transactions from './components/Transactions'
-import Operations from './components/Operations'
-import Breakdown from './components/Breakdown'
+import Transactions from "./components/Transactions";
+import Operations from "./components/Operations";
+import Breakdown from "./components/Breakdown";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <Router>
       <div>
+        <Navbar/>
         <Route exact path="/" render={() => <Transactions />}></Route>
         <Route exact path="/operations" render={() => <Operations />}></Route>
         <Route exact path="/breakdown" render={() => <Breakdown />}></Route>
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import '../styles/navbar.css'
 
 export default function Navbar() {
-    return (
-      <div className="navbar">
-        
-      </div>
-    );
-  }
+  return (
+    <div className="navbar">
+      <ul>
+        <li>
+          <Link to="/">Transactions</Link>
+        </li>
+        <li>
+          <Link to="/operations">Operations</Link>
+        </li>
+        <li>
+          <Link to="/breakdown">Breakdown</Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
